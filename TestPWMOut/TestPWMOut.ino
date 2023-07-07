a64f57195b8eeddd9d5b5e476f95413c91c0dae7
lecheng <lechenggood@foxmail.com>
2023-07-07 09:26:21 +0800

    pwm测试完善

--- TestPWMOut/TestPWMOut.ino
+++ TestPWMOut/TestPWMOut.ino
@@ -1,7 +1,10 @@
 #include <MIDI.h>
+// #include <FlexiTimer2.h>
 #include <SPI.h>                 //DAC通信用
 MIDI_CREATE_DEFAULT_INSTANCE();  //MIDIライブラリを有効启用MIDI库
 
+//2 4 7 gate
+//3 5 6 CV
 const int LDAC = 9;  //SPI trans setting
 int note_no = 0;     //noteNo=21(A0)～60(A5) total 61,マイナスの値を取るのでint 因为取负值，所以int
 
@@ -13,177 +16,62 @@ long after_bend_pitch = 0;
 byte note_on_count = 0;       //当多个音符打开且其中一个音符关闭时，最后一个音符不消失。
 unsigned long trigTimer = 0;  //for gate ratch
 
-byte clock_count = 0;
-byte clock_max = 24;  //clock_max change by knob setting
-byte clock_on_time = 0;
-int clock_rate = 0;  //knob CVin
-int total_mod = 0;
-
-const long cv[61] = {  // V/OCT LSB for DAC
-  0, 68, 137, 205, 273, 341, 410, 478, 546, 614, 683, 751,
-  819, 887, 956, 1024, 1092, 1161, 1229, 1297, 1365, 1434, 1502, 1570,
-  1638, 1707, 1775, 1843, 1911, 1980, 2048, 2116, 2185, 2253, 2321, 2389,
-  2458, 2526, 2594, 2662, 2731, 2799, 2867, 2935, 3004, 3072, 3140, 3209,
-  3277, 3345, 3413, 3482, 3550, 3618, 3686, 3755, 3823, 3891, 3959, 4028, 4095
-};
+int p3 = 0, p5 = 0, p6 = 0;
 
 void setup() {
   Serial.begin(115200);
 
   pinMode(LDAC, OUTPUT);  //DAC trans
   pinMode(SS, OUTPUT);    //DAC trans
-  pinMode(3, OUTPUT);     //PWM out
+  pinMode(2, OUTPUT);     //CLK_OUT
   pinMode(4, OUTPUT);     //CLK_OUT
-  pinMode(5, OUTPUT);     //GATE_OUT
+  pinMode(7, OUTPUT);     //CLK_OUT
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
 
   MIDI.begin(1);  // MIDI CH1をlisten
 
-  SPI.begin();
-  SPI.setBitOrder(MSBFIRST);            // bit order
-  SPI.setClockDivider(SPI_CLOCK_DIV4);  // クロック(CLK)をシステムクロックの1/4で使用(16MHz/4)
-  SPI.setDataMode(SPI_MODE0);           // クロック極性０(LOW)　クロック位相０
   delay(50);
+
+  // TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  // TCCR2B = _BV(CS22);
 }
 
-int testMod = 0;
+int old_p3 = 0;
+int filter_int = 10;
+int isMidi = 0;
 int testMod2 = 0;
+
 void loop() {
-  // Serial.println(" ");
-  // Serial.println("  ");
-
-  testMod2++;
-  if (testMod2 > 200) {
-    testMod2 = 0;
-  }
-  analogWrite(3, testMod2);
-
-  // analogWrite(3, total_mod);  //给引脚3映射mod控制
-  //-----------------------------clock_rate setting----------------------------
-  clock_rate = analogRead(1);  //read knob voltage
-
-  if (clock_rate < 256) {
-    clock_max = 24;  //slow
-  } else if (clock_rate < 512 && clock_rate >= 256) {
-    clock_max = 12;
-  } else if (clock_rate < 768 && clock_rate >= 512) {
-    clock_max = 6;
-  } else if (clock_rate >= 768) {
-    clock_max = 3;  //fast
-  }
-
-  //-----------------------------gate ratch----------------------------
-  if (note_on_count != 0) {
-    Serial.println("1111111111111");
-    // digitalWrite(5, HIGH);//test
-
-    if ((millis() - trigTimer <= 20) && (millis() - trigTimer > 10)) {
-      // digitalWrite(5, LOW);
-    }
-    if ((trigTimer > 0) && (millis() - trigTimer > 20)) {
-      // digitalWrite(5, HIGH);
-    }
-  }
-  // else{
-  // digitalWrite(5, LOW);//test
+  //循环测试
+  // testMod2++;
+  // if (testMod2 > 200) {
+  //   testMod2 = 0;
   // }
-
+  // analogWrite(3, testMod2);
   //-----------------------------midi operation----------------------------
   if (MIDI.read()) {  // 如果频道1有信号的话
-    // digitalWrite(5, HIGH);//test
 
+    digitalWrite(2, HIGH);  //Gate》HIGH
     MIDI.setInputChannel(1);
     switch (MIDI.getType()) {
 
-      case midi::NoteOn:  //if NoteOn
-        note_on_count++;
-        trigTimer = millis();
-        note_no = MIDI.getData1() - 21;  //note number
-
-        if (note_no < 0) {
-          note_no = 0;
-        } else if (note_no >= 61) {
-          note_no = 60;
-        }
-
-        // digitalWrite(5, HIGH);  //Gate》HIGH
-        OUT_CV(cv[note_no]);  //V/OCT LSB for DAC》参照
-        break;
-
-
-      case midi::NoteOff:  //if NoteOff 关闭后
-
-        note_on_count--;
-        if (note_on_count == 0) {
-          // digitalWrite(5, LOW);  //Gate 》LOW
-        }
-        break;
-
-
       case midi::ControlChange:
-        OUT_MOD(MIDI.getData2() << 5);  //0-4095
-        break;
+        switch (MIDI.getData1()) {  //D3 D5 D6
+          default:
+          case 1:  //modulation
+            p3 = MIDI.getData2() << 1;
+            // if (p3 - old_p3 < filter_int && p3 - old_p3 > -filter_int)
+            //   analogWrite(3, p3);
+            // old_p3 = p3;
+            // OUT_PWM(3, MIDI.getData2() << 3);
 
-
-      case midi::Clock:
-        clock_count++;
-
-        if (clock_count >= clock_max) {
-          clock_count = 0;
+            break;
         }
-
-        if (clock_count == 1) {
-          digitalWrite(4, HIGH);
-        } else if (clock_count != 1) {
-          digitalWrite(4, LOW);
-        }
-        break;
-
-
-      case midi::Stop:
-        clock_count = 0;
-        // digitalWrite(5, LOW);  //Gate》LOW
-        break;
-
-
-      case midi::PitchBend:
-        bend_lsb = MIDI.getData1();  //LSB
-        bend_msb = MIDI.getData2();  //MSB
-        bend_range = bend_msb;       //0 to 127
-
-        if (bend_range > 64) {
-          after_bend_pitch = cv[note_no] + cv[note_no] * (bend_range - 64) * 4 / 10000;
-          OUT_CV(after_bend_pitch);
-        }
-
-        else if (bend_range < 64) {
-          after_bend_pitch = cv[note_no] - cv[note_no] * (64 - bend_range) * 4 / 10000;
-          OUT_CV(after_bend_pitch);
-        }
-        break;
     }
   } else {
-    // digitalWrite(5, LOW);//test
+    digitalWrite(2, LOW);  //Gate》HIGH
   }
-}
-
-
-//DAC_CV output
-void OUT_CV(int cv) {
-  digitalWrite(LDAC, HIGH);
-  digitalWrite(SS, LOW);
-  SPI.transfer((cv >> 8) | 0x30);  // H0x30=OUTA/1x
-  SPI.transfer(cv & 0xff);
-  digitalWrite(SS, HIGH);
-  digitalWrite(LDAC, LOW);
-}
-
-//DAC_MOD output
-void OUT_MOD(int mod) {
-  total_mod = mod / 4;
-  digitalWrite(LDAC, HIGH);
-  digitalWrite(SS, LOW);
-  SPI.transfer((mod >> 8) | 0xB0);  // H0xB0=OUTB/1x
-  SPI.transfer(mod & 0xff);
-  digitalWrite(SS, HIGH);
-  digitalWrite(LDAC, LOW);
+  analogWrite(3, p3);
 }
