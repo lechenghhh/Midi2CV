ffcc398a5c06fa67927a0e733bdfd649a8c8be7f
lecheng <lechenggood@foxmail.com>
2024-03-26 15:10:19 +0800

    增加通道选择功能 Signed-off-by: lecheng <lechenggood@foxmail.com>

--- TRS-V3.2/TRS-V3.2.ino
+++ TRS-V3.2/TRS-V3.2.ino
@@ -1,47 +1,42 @@
 #include <MIDI.h>
-// #include <FlexiTimer2.h>
 #include <SPI.h>  //DAC通信用
+// #include <FlexiTimer2.h>
 
-#define CLOCK_PIN 2  //CLK
-#define GATA1_PIN 4  //Gate
-#define GATA2_PIN 7
-#define CV1_PIN 3
-#define CV2_PIN 5
-#define CV3_PIN 6
+#define CONFIG1_PIN 8   //配置1
+#define CONFIG2_PIN 12  //配置2
+#define CLOCK_PIN 2     //CLK
+#define GATA1_PIN 4     //Gate
+#define GATA2_PIN 7     //Gate2
+#define CV1_PIN 3       //CV1
+#define CV2_PIN 5       //CV2
+#define CV3_PIN 6       //CV3
 
-MIDI_CREATE_DEFAULT_INSTANCE();  //MIDIライブラリを有効启用MIDI库
+MIDI_CREATE_DEFAULT_INSTANCE();  //启用MIDI库
 
-//2 4 7 gate
-//3 5 6 CV
+byte ch1 = 1;
+byte ch2 = 2;
+
 const byte LDAC = 9;  //SPI trans setting
-
 byte bend_range = 0;
 byte bend_msb = 0;
 byte bend_lsb = 0;
 int after_bend_pitch = 0;
-
-byte note_no1 = 0;  //noteNo=21(A0)～60(A5) total 61,マイナスの値を取るのでint 因为取负值，所以int
-byte note_no2 = 0;  //noteNo=21(A0)～60(A5) total 61,マイナスの値を取るのでint 因为取负值，所以int
-byte poly_on1 = 0;  //noteNo=21(A0)～60(A5) total 61,マイナスの値を取るのでint 因为取负值，所以int
-byte poly_on2 = 0;  //noteNo=21(A0)～60(A5) total 61,マイナスの値を取るのでint 因为取负值，所以int
-
-
+byte note_no1 = 0;        //noteNo=21(A0)～60(A5) total 61,マイナスの値を取るのでint 因为取负值，所以int
+byte note_no2 = 0;        //noteNo=21(A0)～60(A5) total 61,マイナスの値を取るのでint 因为取负值，所以int
+byte poly_on1 = 0;        //noteNo=21(A0)～60(A5) total 61,マイナスの値を取るのでint 因为取负值，所以int
+byte poly_on2 = 0;        //noteNo=21(A0)～60(A5) total 61,マイナスの値を取るのでint 因为取负值，所以int
 byte note_on_count1 = 0;  //当多个音符打开且其中一个音符关闭时，最后一个音符不消失。
 byte note_on_count2 = 0;  //当多个音符打开且其中一个音符关闭时，最后一个音符不消失。
 byte poly_on_count = 0;   //当多个音符打开且其中一个音符关闭时，最后一个音符不消失。
-
-// unsigned byte trigTimer = 0;  //for gate ratch
-
 byte clock_count0 = 0;
 byte clock_count1 = 0;
 byte clock_count2 = 0;
 byte clock_max = 24;  //clock_max change by knob setting
 byte clock_on_time = 0;
 int clock_rate = 0;  //knob CVin
-
 byte tmp_last_note1 = -1, tmp_last_note2 = -1;
-
-byte cc_mode = 0;         //用于更改当前cc映射模式
+byte cc_mode = 0;  //用于更改当前cc映射模式
+byte Master = 1;
 float OCT_CONST = 68.25;  // V/OCT 常量
 // const int V_OCT[61] = {   // V/OCT LSB for DAC
 //   0, 68, 137, 205, 273, 341, 410, 478, 546, 614, 683, 751,
@@ -65,6 +60,8 @@ void setup() {
   pinMode(CV1_PIN, OUTPUT);
   pinMode(CV2_PIN, OUTPUT);
   pinMode(CV3_PIN, OUTPUT);
+  pinMode(CONFIG1_PIN, INPUT_PULLUP);
+  pinMode(CONFIG2_PIN, INPUT_PULLUP);
 
   MIDI.begin(MIDI_CHANNEL_OMNI);  // MIDI CH ALL Listen
 
@@ -72,6 +69,16 @@ void setup() {
   SPI.setBitOrder(MSBFIRST);            // bit order
   SPI.setClockDivider(SPI_CLOCK_DIV4);  // クロック(CLK)をシステムクロックの1/4で使用(16MHz/4)
   SPI.setDataMode(SPI_MODE0);           // クロック極性０(LOW)　クロック位相０
+
+  Master = digitalRead(CONFIG1_PIN);  //读取d8跳线帽 默认1 插上0
+  if (Master == 1) {                  //频道选择
+    ch1 = 1;
+    ch2 = 2;
+  }
+  if (Master == 0) {
+    ch1 = 3;
+    ch2 = 4;
+  }
 
   digitalWrite(CLOCK_PIN, HIGH);
   digitalWrite(GATA1_PIN, HIGH);
@@ -91,8 +98,6 @@ void setup() {
 void loop() {
   // Serial.println(" ");
   //-----------------------------clock_rate setting----------------------------
-  // clock_rate = 1;  //read knob voltage
-
   if (clock_rate < 256) {
     clock_max = 24;  //slow
   } else if (clock_rate < 512 && clock_rate >= 256) {
@@ -131,7 +136,7 @@ void loop() {
         bend_msb = MIDI.getData2();  //MSB
         bend_range = bend_msb;       //0 to 127
 
-        if (cc_mode != 3) {          //双通道模式
+        if (cc_mode != 3) {  //双通道模式
           if (bend_range > 64) {
             after_bend_pitch = OCT_CONST * note_no1 + OCT_CONST * note_no1 * (bend_range - 64) * 4 / 10000;
             OUT_CV1(after_bend_pitch);
@@ -217,7 +222,7 @@ void loop() {
         break;  //ControlChange
     }
 
-    if (MIDI.getChannel() == 1) {  //MIDI CH1
+    if (MIDI.getChannel() == ch1) {  //MIDI CH1
       switch (MIDI.getType()) {
         case midi::NoteOn:     //if NoteOn
           if (cc_mode != 3) {  //双通道模式
@@ -278,12 +283,11 @@ void loop() {
               digitalWrite(7, LOW);  //Gate 》LOW
             }
           }
-
           break;
       }
     }
 
-    if (MIDI.getChannel() == 2) {  //MIDI CH2
+    if (MIDI.getChannel() == ch2) {  //MIDI CH2
       switch (MIDI.getType()) {
         case midi::NoteOn:     //if NoteOn
           if (cc_mode != 3) {  //双通道模式
