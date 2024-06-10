3b482c0fac6351752cc18be22f0daa3599c1754f
lc-hp <951827860@qq.com>
2024-06-10 17:28:34 +0800

    随机长度完成 但是mstimer可能使d3被占用 Signed-off-by: lc-hp <951827860@qq.com>

--- Midi2CV_v4.0/random_trig.h
+++ Midi2CV_v4.0/random_trig.h
@@ -1,16 +1,25 @@
+#include <MsTimer2.h>
+
 int trig_clk_in = 0;
+
+void gate2Stop() {
+  digitalWrite(GATE2_PIN, 0);
+}
 
 void triggerOn() {
   int randomNum = random(0, 255);
+  //rand trig
   if (randomNum < 192)
     OUT_CV1(4095);
   if (randomNum < 128)
     digitalWrite(GATE1_PIN, 1);
   if (randomNum < 64)
     OUT_CV2(4095);
-  if (randomNum < 32)
-    digitalWrite(GATE2_PIN, 1);
-
+  //rand gate length
+  digitalWrite(GATE2_PIN, 1);
+  MsTimer2::set(random(0, 1024), gate2Stop);  // 0-1024 ms //此功能可能导致d3 cv1无效
+  MsTimer2::start();
+  //rabd level
   analogWrite(CV1_PIN, randomNum);         //8bit 0-256  2^8
   analogWrite(CV2_PIN, 255 - randomNum);   //inv
   analogWrite(CV3_PIN, (randomNum >> 1));  //7bit 0-128
@@ -20,7 +29,7 @@ void triggerOff() {
   OUT_CV1(0);
   digitalWrite(GATE1_PIN, 0);
   OUT_CV2(0);
-  digitalWrite(GATE2_PIN, 0);
+  // digitalWrite(GATE2_PIN, 0);
 }
 
 void triggerListener() {
